16d03c14f1bb770237d640fdfc1e50a797f5c93c
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-11-04 17:26:57 +0530

    Enhance loading state in PrivateRoutes with a spinner for better user experience

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,11 +1,16 @@
 import { useAuth } from "../context/authContext";
 import { Outlet, Navigate } from "react-router-dom";
+import { Loader2 } from "lucide-react";
 
 export default function PrivateRoutes() {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-900">
+        <Loader2 className="h-32 w-32 animate-spin text-purple-400" />
+      </div>
+    );
   }
 
   return user ? <Outlet /> : <Navigate to="/login" />;
